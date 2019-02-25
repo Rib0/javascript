c59418dc86d57a36c1117d41c1e2b70a820ef3d9
Nikita <nikitsidenk@mail.ru>
2019-02-25 21:41:33 +0300

    oop fix

--- js Games/game with time/file.js
+++ js Games/game with time/file.js
@@ -1,93 +1,106 @@
-var timer = document.getElementById("timer");
-var time = document.getElementById("time");
-var start = document.getElementById("start");
-var wrapper = document.getElementById("wrapper");
-var startGame = document.getElementById("start-game");
-var restart = document.getElementById("restart");
-var blocks = document.getElementsByClassName("block");
-var current;
+class Game {
+    constructor () {
+        const cellsNumber = document.getElementById('cells');
+        this.timerId = null;
+        this.currentValue = 1;
+        this.maxValue;
+        this.started = false;
 
-start.addEventListener("click", startGm);
-restart.addEventListener("click", startGm);
+        this.generateField = this.generateField.bind(this);
+        this.onClick = this.onClick.bind(this);
 
-function startGm() {
-    var same;
-    timer.innerHTML = "Времени осталось: 75";
-    StopTm();
-    startTm();
-    current = 1;
-    for (var i = 0; i < blocks.length; i++) {
-        blocks[i].style.background = "white";
+        cellsNumber.onkeydown = this.generateField;// вынести все навешивания обработчиков в отдельную функцию и снятия тоже
     }
-    for (var i = 0; i < blocks.length; i++) {
-        blocks[i].addEventListener("click", blockClick);
+
+    getRandomValue (max) {
+        return Math.floor(Math.random() * max) + 1;
     }
-    for (var i = 0; i < blocks.length; i++) {
-        blocks[i].innerHTML = "";
+
+    getRandomValueColor () {
+        return [
+            Math.floor(Math.random() * 210) + 1, 
+            Math.floor(Math.random() * 210) + 1, 
+            Math.floor(Math.random() * 210) + 1,
+        ]
     }
-    wrapper.style.display = "none";
-    startGame.style.display = "block";
-    for (var i = 0; i < blocks.length;) {
-        same = false;
-        var numb = randomize();
-        for (var j = 0; j < blocks.length; j++) {
-            if (blocks[j].innerHTML == numb) {
-                same = true;
-                break;
+
+    isLetter (keyCode) {
+        var char = String.fromCharCode(keyCode);
+        return !!char.search(/[a-z]|[а-я]/i);
+    }
+
+    generateField (e) {
+        if (!this.isLetter(e.keyCode)) e.preventDefault();
+        if (e.keyCode !== 13 || e.value === '') return;    
+
+        const value = parseInt(e.target.value);
+        const table = document.createElement('table');
+        const gameWrapper = document.querySelector('.game');
+        const tableValues = [];
+        this.maxValue = Math.pow(value, 2);
+        this.startGame = this.initTime(60);
+
+        for (let i = 0; i < value; i++) {
+            const row = document.createElement('tr');
+            for (let j = 0; j < value; j++) {
+                const cell = document.createElement('td');
+                let randomValue = this.getRandomValue(Math.pow(value, 2));
+                while (tableValues.includes(randomValue)) {
+                    randomValue = this.getRandomValue(Math.pow(value, 2));
+                }
+                tableValues.push(randomValue);
+                cell.textContent = randomValue;
+                cell.style.color = `RGB(${this.getRandomValueColor()})`;
+                row.appendChild(cell);
             }
+            table.appendChild(row);
+            table.onclick = this.onClick;
         }
-        if (same == false) {
-            blocks[i].style.fontSize = GetRandomSize() + "px";
-            blocks[i].style.color = "rgb(" + GerRdClr() + "," + GerRdClr() + "," + GerRdClr() + ")";
-            blocks[i].innerHTML = numb;
-            i++
+        gameWrapper.appendChild(table);
+        e.target.style.display = 'none';
+    }
+
+    onClick (e) {
+        if (e.target.tagName !== 'TD') return;
+
+        if (!this.started) 
+            this.startGame();
+        const { target } = e;
+        const value = parseInt(target.textContent);
+        if (value !== this.currentValue) return;
+        target.style.backgroundColor = 'red';
+        const currentValue = this.currentValue++;
+        this.checkTheEndGame({currentValue});
+    }
+
+    initTime (defaultTime) {
+        const timer = document.querySelector('.timer');
+        let currentTime = timer.textContent = defaultTime;
+
+        return () => {
+            this.started = true;
+            this.timerId = setInterval(()=> {
+                timer.textContent = currentTime;
+                this.checkTheEndGame({currentTime});
+                currentTime--;
+            }, 1000)
         }
+    }
+
+    checkTheEndGame ({currentTime, currentValue}) {
+        const gameRes = document.getElementById('gameResult');
+        const table = document.querySelector('table');
+        if (currentTime === 0) {
+            clearInterval(this.timerId);
+            gameRes.textContent = 'Вы проиграли';
+            table.onclick = null;
+        }
+        if (currentValue === this.maxValue) {
+            clearInterval(this.timerId);
+            gameRes.textContent = 'Вы выиграли';
+            table.onclick = null;      
+        }    
     }
 }
 
-function blockClick() {
-    if (this.innerHTML == current && this.className == "block") {
-        this.style.background = "#c00";
-        current++;
-    }
-    if (current == 26) {
-        timer.innerHTML = "Вы выиграли";
-        StopTm();
-        for (var i = 0; i < blocks.length; i++) {
-            blocks[i].removeEventListener("click", blockClick);
-        }
-    }
-}
-
-function randomize() {
-    return Math.floor(Math.random() * 25) + 1;
-}
-
-function GetRandomSize() {
-    return Math.floor(Math.random() * (50 - 20 + 1)) + 20;
-}
-
-function GerRdClr() {
-    return Math.floor(Math.random() * (200 - 50 + 1)) + 50;
-}
-
-function startTm() {
-    window.timerId = window.setInterval(GoTime, 1000);
-}
-
-function GoTime() {
-    var tm = timer.innerHTML.split(" ");
-    timer.innerHTML = "Времени осталось: " +
-        (tm[2] - 1);
-    if (tm[2] == 1) {
-        timer.innerHTML = "Вы проиграли";
-        StopTm();
-        for (var i = 0; i < blocks.length; i++) {
-            blocks[i].removeEventListener("click", blockClick);
-        }
-    }
-}
-
-function StopTm() {
-    window.clearInterval(window.timerId);
-}
+const game = new Game();
